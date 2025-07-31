ca0a4b4c23dc397c99db3748624135eb7f3abce5
SamarthGandhi2004 <samarthgandhi30@gmail.com>
2025-07-31 08:21:47 +0530

    session1

---
+++ session1/app.js
@@ -0,0 +1,19 @@
+"use strict";
+// var a=12;
+// var b=a;
+// var a=[1,2,3,45,5,4];
+// var b=a;
+// var a=[1,2,3,"samarth"];
+// var a:number[]=[1,2,3,4,{"fsdf"}]
+// var a:[number,string]=[22,"fsdf"]
+// enum iCard{
+//     NAME="name",
+//     ROLL="student",
+//     COLLEGE="university"
+// }
+// iCard.COLLEGE
+// let a:unknown;
+// a='12';
+// a="samarth";
+// if(typeof a ==='string')
+// a.toUpperCase();
---
+++ session1/app.ts
@@ -0,0 +1,28 @@
+// var a=12;
+
+// var b=a;
+
+// var a=[1,2,3,45,5,4];
+// var b=a;
+
+
+// var a=[1,2,3,"samarth"];
+
+// var a:number[]=[1,2,3,4,{"fsdf"}]
+
+// var a:[number,string]=[22,"fsdf"]
+
+// enum iCard{
+//     NAME="name",
+//     ROLL="student",
+//     COLLEGE="university"
+// }
+
+// iCard.COLLEGE
+
+
+// let a:unknown;
+// a='12';
+// a="samarth";
+// if(typeof a ==='string')
+// a.toUpperCase();
